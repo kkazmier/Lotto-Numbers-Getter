2d978db69d06309ae5d1408abe272bdd9db57897
kkazm <s14005@pjwstk.edu.pl>
2022-01-30 14:37:40 +0100

    Initial commit.

---
+++ src/test/java/com/lottoresultsgetter/LottoResultsGetterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lottoresultsgetter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LottoResultsGetterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
