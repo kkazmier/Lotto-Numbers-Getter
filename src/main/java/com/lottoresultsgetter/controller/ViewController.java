993d74872835ee14bec3cf3ac93a6e0e52e39348
kkazm <s14005@pjwstk.edu.pl>
2022-02-01 15:08:49 +0100

    First implementation.

--- src/main/java/com/lottoresultsgetter/controller/ViewController.java
+++ src/main/java/com/lottoresultsgetter/controller/ViewController.java
@@ -1,5 +1,15 @@
 package com.lottoresultsgetter.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class ViewController {
+    @GetMapping({"/", "/home"})
+    public String home(Model model) {
+        return "home";
+    }
+
 
 }
